8750533a12848850add9a1073e94ff97e476075a
ANDERSOUNDZ <andersonmikol@live.com>
2019-02-26 10:36:18 -0500

    Subiendo nuevo cambio

--- src/app/service/shopping-cart-service.service.ts
+++ src/app/service/shopping-cart-service.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { detailsProductos } from '../model/descriptionProducts';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingCartServiceService {
+
+  public cartData: detailsProductos [] = [];
 
   constructor() { 
 
